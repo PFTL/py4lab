a5acb4a1115286cdce2db8fbd8380a6051a0d408
Aquiles Carattino <aqui.carattino@gmail.com>
2020-04-25 17:20:06 +0200

    Chapter 04

---
+++ ch_04/PythonForTheLab/Controller/dummy_controller.py
@@ -0,0 +1,8 @@
+print('This is the dummy Controller')
+
+def dummy_example():
+    print('This is a function in the dummy Controller')
+
+if __name__ == '__main__':
+    print('This is printed only from __main__')
+    dummy_example()
---
+++ ch_04/PythonForTheLab/Model/analog_daq.py
@@ -0,0 +1,2 @@
+class AnalogDaq:
+    pass
